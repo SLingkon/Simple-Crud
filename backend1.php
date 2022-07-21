cc4caa5da6284ac1a80330ea2ae12393cd7ec20f
SLingkon <you@example.com>
2022-07-21 21:35:29 +0600

    database connecting

--- backend1.php
+++ backend1.php
@@ -3,3 +3,11 @@ $conn = mysqli_connect('localhost', 'root', "", 'crudproject');
 extract($_POST);
 
 if (isset($_POST['firstname']) && (isset($_POST['lastname'])) && (isset($_POST['email'])) (isset($_POST['mobile'])) )
+
+
+
+
+
+
+
+
